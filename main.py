9cb0b507d9535d206c1053cd0dc787b2b960f796
Laros123 <you@example.com>
2023-12-30 16:31:35 +0200

    Edited functional factorial

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def factorial(obj: int) -> int:
         return 1
     return factorial(obj-1) * factorial_num
 
+
 while True:
-    print(factorial(int(input('Введите число для факториала \n'))))
+    number = int(input('Введите число для факториала \n'))
+    result = factorial(number)
+    print(f"Факторіал числа {number} дорівнює {result}")
     if str(input('Хотите продолжить? y/n \n')).lower() == 'n':
         exit()
